fb95b32a7951ed66d18774a5678630159449a23d
Christian <minisirhc@gmail.com>
2026-02-05 19:55:19 -0600

    flavorDimensions

--- android/app/src/main/java/com/patatus/axioma/core/di/AppContainer.kt
+++ android/app/src/main/java/com/patatus/axioma/core/di/AppContainer.kt
@@ -1,6 +1,7 @@
 package com.patatus.axioma.core.di
 
 import android.content.Context
+import com.patatus.axioma.BuildConfig
 import com.patatus.axioma.core.network.AuthApiService
 import com.patatus.axioma.features.auth.data.repositories.AuthRepositoryImpl
 import com.patatus.axioma.features.auth.domain.repositories.AuthRepository
@@ -9,13 +10,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class AppContainer(context: Context) {
 
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("https://esparrago.engineer")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
+    private fun createRetrofit(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    private val authretrofit = createRetrofit(BuildConfig.BASE_URL_API)
     private val authApiService: AuthApiService by lazy {
-        retrofit.create(AuthApiService::class.java)
+        authretrofit.create(AuthApiService::class.java)
     }
 
     val authRepository: AuthRepository by lazy {
