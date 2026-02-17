ac44b2fae53c262b960433979567e4d783bcd6dd
Christian <minisirhc@gmail.com>
2026-02-16 22:36:20 -0600

    correcion de model auth

--- backend/app/modules/users/infrastructure/persistence/sql_repository.py
+++ backend/app/modules/users/infrastructure/persistence/sql_repository.py
@@ -2,7 +2,7 @@ from typing import Optional
 from sqlmodel import Session
 from app.modules.users.domain.repository import UserRepository
 from app.modules.users.domain.entities import User
-from app.modules.users.infrastructure.persistence.models import UserModel
+from app.modules.auth.infrastructure.persistence.models import UserModel
 
 class SQLUserRepository(UserRepository):
     def __init__(self, session: Session):
